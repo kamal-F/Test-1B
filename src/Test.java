9557c5cc420b8fb58a8e6ad2038cc2ae279a430d
kamal fauzan <kamalpsrina@gmail.com>
2016-02-01 04:24:38 +0700

    loop

--- src/Test.java
+++ src/Test.java
@@ -2,10 +2,13 @@
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub		
-		for (int i = 0; i < 10; i++) {
-			System.out.println("ok"+i);
+		int i = 0;		
+
+		while (i<10) {
+			System.out.println("ok" + i);
+			i++;
 		}
+
 	}
 
 }
