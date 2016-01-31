25b36095878f9006e3ce8aa4812b7afe5d21edbc
kamal fauzan <kamalpsrina@gmail.com>
2016-02-01 04:18:59 +0700

    use for

--- src/Test.java
+++ src/Test.java
@@ -2,10 +2,11 @@
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub		
+		// for		
 		for (int i = 0; i < 10; i++) {
 			System.out.println("ok"+i);
 		}
+		
 	}
 
 }
