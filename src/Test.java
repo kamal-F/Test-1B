013edb04d5047244c8127cec7c8003e6ce195892
kamal fauzan <kamalpsrina@gmail.com>
2016-01-31 19:39:45 +0700

    change to loop

--- src/Test.java
+++ src/Test.java
@@ -2,9 +2,10 @@
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("ok");
-		System.out.println("ok2");
+		// TODO Auto-generated method stub		
+		for (int i = 0; i < 10; i++) {
+			System.out.println("ok"+i);
+		}
 	}
 
 }
