3e783688be93950291e8a49819f614b70983e07e
kamal fauzan <kamalpsrina@gmail.com>
2016-02-10 05:41:49 +0700

    add encapsulation test git

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,8 @@
 
 public class Test {
 
+	private int k;
+	
 	public static void main(String[] args) {
 		int i = 0;		
 
@@ -8,7 +10,19 @@ public class Test {
 			System.out.println("ok" + i);
 			i++;
 		}
+		
+		Test ObjT = new Test();
+		ObjT.setK(100);
+		System.out.println(ObjT.getK());
 
+	}
+
+	public int getK() {
+		return k;
+	}
+
+	public void setK(int k) {
+		this.k = k;
 	}
 
 }
