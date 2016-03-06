882587183bfbba7f921171a76b79194293357aea
kamal fauzan <kamalonnetx@gmail.com>
2016-03-06 08:59:58 +0700

    ok

--- src/Test2.java
+++ src/Test2.java
@@ -3,6 +3,6 @@ public class Test2 {
 
 	public static void tampil(){
 		System.out.println("Test 2 tampil ok 2 bs");
-		System.out.println("terkendali, ok");
+		System.out.println("terkendali");
 	}
 }
