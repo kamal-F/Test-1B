d8f69ce04f6d5857b0402019b58f863202719df8
kamal fauzan <kamalonnetx@gmail.com>
2016-03-06 09:12:39 +0700

    alt 3, tambah ke

--- src/Test.java
+++ src/Test.java
@@ -6,7 +6,7 @@ public class Test {
 		System.out.println("masukan perulang?");
 		int k = new Scanner(System.in).nextInt();
 		for (int i = 0; i < k; i++) {
-			System.out.println("Test "+ i);
+			System.out.println("Test ke"+ i);
 		}		
 	}
 
